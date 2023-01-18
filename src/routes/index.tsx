1c7a30e976afd050110712b6924b22ed7c9fabd0
Abdul haidar <abdulhdr1@gmail.com>
2023-01-18 08:58:50 -0300

    add gradient bg, negative text

---
+++ src/routes/index.tsx
@@ -0,0 +1,38 @@
+import { component$, useClientEffect$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { Gradient } from "~/assets/gradient";
+
+export default component$(() => {
+	useClientEffect$(() => {
+		const gradient = new Gradient();
+
+		gradient.initGradient("#gradient-canvas");
+	});
+
+	return (
+		<main class="min-h-screen flex flex-col relative bg-slate-900">
+			<canvas
+				id="gradient-canvas"
+				class="fixed inset-0"
+				data-transition-in
+			/>
+			<div class={"relative inset-y-64 font-extrabold text-7xl"}>
+				<h1 class={""}>Reimagine</h1>
+				<h1 class="mix-blend-difference text-white top-20">
+					{" "}
+					everything{" "}
+				</h1>
+			</div>
+		</main>
+	);
+});
+
+export const head: DocumentHead = {
+	title: "Welcome to Qwik",
+	meta: [
+		{
+			name: "description",
+			content: "Qwik site description",
+		},
+	],
+};
